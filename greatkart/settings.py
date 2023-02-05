d4da131390f523d2967c0e2a0dd2670cb3c77272
irfanirshad <84003624+irfanirshad@users.noreply.github.com>
2023-02-06 04:35:35 +0530

    hosts added

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool) # True
 
-ALLOWED_HOSTS = ['greatkart-course-env.eba-invvpaqg.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['greatkart-course-env.eba-ahykkgjp.us-west-2.elasticbeanstalk.com']
 
 # W
 # Application definition
